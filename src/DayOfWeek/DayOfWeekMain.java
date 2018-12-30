b33c3c51de11991776fc9e3993b721d8da06c147
3yebmb <3yebmb@gmail.com>
2018-12-30 16:53:35 +0500

    release

--- src/DayOfWeek/DayOfWeekMain.java
+++ src/DayOfWeek/DayOfWeekMain.java
@@ -6,14 +6,39 @@ public class DayOfWeekMain {
     }
 
     public static void main(final String[] args) {
-        System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+        System.out.println(getWorkingHours(DayOfWeek.SATURDAY));
     }
 
-    public static int getWorkingHours(DayOfWeek day){
-        int hours = 0;
+    public static String getWorkingHours(DayOfWeek day){
+        int hours = 40;
 
-        hours = (day.ordinal()+1)*8;
+        switch (day.ordinal()){
+            case 0 :
+                hours = 40;
+                break;
+            case 1 :
+                hours = 32;
+                break;
+            case 2 :
+                hours = 24;
+                break;
+            case 3 :
+                hours = 16;
+                break;
+            case 4 :
+                hours = 8;
+                break;
+            case 5 :
+                hours = 0;
+                break;
+            case 6 :
+                hours = 0;
+                break;
+        };
 
-        return hours;
+        if (hours != 0)
+            return "До конца недели осталось " + hours + " ч.";
+        else
+            return "Расслабься, сегодня выходной";
     }
 }
