5a79fdd2fd78eaaa411a3adf00ea9034f3ad64da
3yebmb <3yebmb@gmail.com>
2018-12-30 15:46:22 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static final int SIZE = 4;
 
-    public static void main(String[] args) throws MyArraySizeException {
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
 
         String[][] arr = new String[SIZE][SIZE];
         String[][] arr2 = new String[SIZE+1][SIZE+1];
@@ -12,14 +12,31 @@ public class Main {
             arr[i][y] = Integer.toString(i) + Integer.toString(y);
         }
 
-        getArr(arr2);
-        getArr(arr3);
+        //arr[1][3] = "-";
+
+        System.out.println(getArr(arr));
+        //getArr(arr3);
     }
 
-    public static void getArr(String[][] str) throws MyArraySizeException{
+    public static int getArr(String[][] str) throws MyArraySizeException, MyArrayDataException{
+        int sum = 0;
+        int a, b;
+
         if (str.length < SIZE)
             throw new MyArraySizeException("Маловато будет!");
         else if (str.length > SIZE)
             throw new MyArraySizeException("Перебор!");
+
+        for (int i=0; i<SIZE; i++)
+            for (int y=0; y<SIZE; y++){
+                char ch[] = str[i][y].toCharArray();
+                for (char c : ch) {
+                    if (!Character.isDigit(c))
+                        throw new MyArrayDataException("В массиве присутствует неверный симпол, в ячейке ", i, y);
+                    else
+                        sum += Integer.parseInt(str[i][y]);
+                }
+            }
+        return sum;
     }
 }
--- src/MyArrayDataException.java
+++ src/MyArrayDataException.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class MyArrayDataException extends Exception {
+    private int a = 0;
+    private int b = 0;
 
-public class MyArrayDataException {
+    public MyArrayDataException(String message, int a, int b)  {
+        super(message + a + " " + b);
+//        this.a = a;
+//        this.b = b;
+    }
 }
