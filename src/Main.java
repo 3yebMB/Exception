95357da226ac8fa61c74e5dcce320c1d3e994d85
3yebmb <3yebmb@gmail.com>
2018-12-30 13:42:36 +0500

    lesson 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static final int SIZE = 4;
+
+    public static void main(String[] args) throws MyArraySizeException {
+
+        String[][] arr = new String[SIZE][SIZE];
+        String[][] arr2 = new String[SIZE+1][SIZE+1];
+        String[][] arr3 = new String[SIZE-1][SIZE+1];
+
+        for (int i=0; i<SIZE; i++)
+            for (int y=0; y<SIZE; y++){
+            arr[i][y] = Integer.toString(i) + Integer.toString(y);
+        }
+
+        getArr(arr2);
+        getArr(arr3);
+    }
+
+    public static void getArr(String[][] str) throws MyArraySizeException{
+        if (str.length < SIZE)
+            throw new MyArraySizeException("Маловато будет!");
+        else if (str.length > SIZE)
+            throw new MyArraySizeException("Перебор!");
+    }
 }
