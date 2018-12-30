133b189076dd3a2d02cced373b00f549ffb359ff
3yebmb <3yebmb@gmail.com>
2018-12-30 16:38:55 +0500

    implementation

--- src/DayOfWeek/DayOfWeekMain.java
+++ src/DayOfWeek/DayOfWeekMain.java
@@ -1,4 +1,19 @@
 package DayOfWeek;
 
 public class DayOfWeekMain {
+    public enum DayOfWeek {
+        MONDAY, TUESDAY, WESNESDAY, THUSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    public static void main(final String[] args) {
+        System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+    }
+
+    public static int getWorkingHours(DayOfWeek day){
+        int hours = 0;
+
+        hours = (day.ordinal()+1)*8;
+
+        return hours;
+    }
 }
